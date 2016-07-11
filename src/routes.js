d5ed993ccb84e32b80cce485759ba36ded0348fc
Muschina <mus_china@mail.ru>
2016-07-11 08:21:45 +0300

    refactor routing

--- src/routes.js
+++ src/routes.js
@@ -6,16 +6,13 @@ import Domino from './components/domino/domino';
 import SchoolCalc from './components/schoolCalc/schoolCalc';
 import Home from './components/home/home';
 import NotFound from './components/notFound/notFound';
-import Grade from './components/schoolCalc/grade';
 
 export const routes = (
   <div>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='/domino' component={Domino} />
-      <Route path='/school_GPA_calculator' component={SchoolCalc}>
-        <Route path='/grade' component={Grade} />
-      </Route>
+      <Route path='/school_GPA_calculator' component={SchoolCalc} />
     </Route>
     <Route path='*' component={NotFound} />
   </div>
